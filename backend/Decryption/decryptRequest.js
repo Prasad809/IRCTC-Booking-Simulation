8c08e85778a63d338fb1471d043ea3230d7b7d2a
prasad <raoprasadamballa@gmail.com>
2026-09-06 13:24:45 +0530

    OAEP to RSA

--- backend/Decryption/decryptRequest.js
+++ backend/Decryption/decryptRequest.js
@@ -52,12 +52,10 @@ function decryptRSA(encryptedText) {
   const decrypted = crypto.privateDecrypt(
     {
       key: privateKey,
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-      oaepHash: "sha256"
+      padding: crypto.constants.RSA_PKCS1_PADDING,
     },
     Buffer.from(encryptedText, "base64")
   );
-
   return decrypted.toString("utf8");
 }
 
