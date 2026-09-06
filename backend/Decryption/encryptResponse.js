e9b0d01e05b9a392efbc31f396c3fd5cd4443bb0
prasad <raoprasadamballa@gmail.com>
2026-09-06 12:47:50 +0530

    keys normall

--- backend/Decryption/encryptResponse.js
+++ backend/Decryption/encryptResponse.js
@@ -10,7 +10,19 @@ const encType = process.env.ENCTYPE || "RSA";
 
 const AES_KEY = "12345678901234567890123456789012";
 
-const publicKey = process.env.publicKey?.replace(/\\n/g, "\n") || fs.readFileSync(path.join(__dirname, "../Keys/public.pem"),"utf8");
+const publicKey = process.env.publicKey?.replace(/\\n/g, "\n") 
+|| fs.readFileSync(path.join(__dirname, "../Keys/public.pem"),"utf8")
+|| `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxGYFtcchYLNiOlZcAV8q
+JJtlil38ijWHg0b1VjV5rss9wVtW11tHQkm94MKyaklbwHf7mKwKvYyd0jWJIc7g
+TOkGEp066sMaJVCKJypnH2rAvNUhUoFcFVfM/3EKjrI7Cmlg6cyav1H9GTIEX6cX
+eMxe4g0C5CFkvhi8dD+znSrvXoC6UXHv6Vhe6qlAwx/rePh5X9uK7oB091ig3wEH
+zOPa9O/PKIlHfLa4MKTeA341RdEiC1OT+RTwMSbypJ333h5ngm1EINcJBUk/W48T
+i44QHvkdQFfHRDD9D7fXNTROjKZ3BSIRBptIWZdgMd0UM7rnKy34HXXoVz1yf4BF
+owIDAQAB
+-----END PUBLIC KEY-----`
+;
+console.log(publicKey,"publicKey")
 
 function encryptAES(data) {
     return CryptoJS.AES.encrypt(
